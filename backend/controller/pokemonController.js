2f89650d99ab4543e70e8608387723550a0dee66
Kristel Dawn <113149111+kristeldawn12@users.noreply.github.com>
2023-05-22 14:50:33 +0100

    feat: re-submitted and added error handling

--- backend/controller/pokemonController.js
+++ backend/controller/pokemonController.js
@@ -5,28 +5,39 @@ const Pokemon = require("../models/pokemonModel.js");
 // Get pokemon
 // @route GET /pokemon
 const getPokemon = asyncHandler(async (req, res) => {
-  const pokemons = await Pokemon.find().sort({ name: 1 });
-
-  res.status(200).json(pokemons);
+  try {
+    const pokemons = await Pokemon.find().sort({ name: 1 });
+    res.status(200).json({
+      message: "Pokemon fetched successfully",
+      data: pokemons,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // GET limit pokemon using params
-//  @route GET /pokemon/:limit
+//  @route GET /pokemon/limit/:limit
 const limitPokemon = asyncHandler(async (req, res) => {
-  const limit = parseInt(req.params.limit);
+  try {
+    const limit = parseInt(req.params.limit);
 
-  if (isNaN(limit)) {
-    res.status(400);
-    throw new Error("Invalid limit parameter");
+    if (isNaN(limit)) {
+      res.status(400);
+      throw new Error("Invalid limit parameter");
+    }
+    const pokemons = await Pokemon.find().limit(limit);
+    res.status(200).json({
+      message: "Pokemon fetched successfully",
+      data: pokemons,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  const pokemons = await Pokemon.find().limit(limit);
-
-  res.status(200).json(pokemons);
 });
 
 // GET pokemon by pagination
-// @route GET /pokemon?page=<number>&limit=<number>
+// @route GET /pokemon/rate-limit?limit=<number>&skip=<number>
 const getPokemonByLimitOffset = asyncHandler(async (req, res) => {
   let limit = parseInt(req.query.limit);
   const offset = parseInt(req.query.skip);
@@ -58,86 +69,108 @@ const getPokemonByLimitOffset = asyncHandler(async (req, res) => {
 // GET pokemon by name
 // @route GET /pokemon/name/:name
 const getPokemonbyName = asyncHandler(async (req, res) => {
-  const pokemonName = req.params.name;
-  const pokemon = await Pokemon.find({
-    name: { $regex: new RegExp(pokemonName, "i") },
-  });
-  if (pokemon) {
-    res.send(pokemon);
-  } else {
-    res
-      .status(404)
-      .send({ error: `Pokemon with name ${pokemonName} not found` });
+  try {
+    const pokemonName = req.params.name;
+    const pokemon = await Pokemon.find({
+      name: { $regex: new RegExp(pokemonName, "i") },
+    });
+
+    if (pokemon.length > 0) {
+      // Check if the array contains any Pokémon
+      res.send(pokemon);
+    } else {
+      res
+        .status(404)
+        .send({ error: `Pokemon with name ${pokemonName} not found` });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
 // GET pokemon by type
 // @route GET /pokemon/type/:type
 const getPokemonByType = asyncHandler(async (req, res) => {
-  const pokemonType = req.params.type;
-  const pokemon = await Pokemon.find({
-    type: { $in: [new RegExp(pokemonType, "i")] },
-  });
-  if (pokemon.length > 0) {
-    res.send(pokemon);
-  } else {
-    res
-      .status(404)
-      .send({ error: `Pokemon with type ${pokemonType} not found` });
+  try {
+    const pokemonType = req.params.type;
+    const pokemon = await Pokemon.find({
+      type: { $in: [new RegExp(pokemonType, "i")] },
+    });
+    if (pokemon.length > 0) {
+      res.send(pokemon);
+    } else {
+      res
+        .status(404)
+        .send({ error: `Pokemon with type ${pokemonType} not found` });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
 // Set pokemon
 // @route POST /pokemon
 const setPokemon = asyncHandler(async (req, res) => {
-  if (!req.body.name) {
-    res.status(400);
-    throw new Error("Please add a text field");
+  try {
+    if (!req.body.name) {
+      res.status(400);
+      throw new Error("Please add a text field");
+    }
+
+    const pokemon = await Pokemon.create({
+      name: req.body.name,
+      img: req.body.img,
+      type: req.body.type,
+      stats: req.body.stats,
+      damages: req.body.damages,
+      misc: req.body.misc,
+    });
+
+    res.status(200).json(pokemon);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  const pokemon = await Pokemon.create({
-    name: req.body.name,
-    img: req.body.img,
-    type: req.body.type,
-    stats: req.body.stats,
-    damages: req.body.damages,
-    misc: req.body.misc,
-  });
-
-  res.status(200).json(pokemon);
 });
 
 // Update pokemon
 // @route PUT /pokemon/:id
 const updatePokemon = asyncHandler(async (req, res) => {
-  const pokemon = await Pokemon.findById(req.params.id);
+  try {
+    const pokemon = await Pokemon.findById(req.params.id);
 
-  if (!pokemon) {
-    res.status(400);
-    throw new Error("Pokemon not found");
+    if (!pokemon) {
+      res.status(400);
+      throw new Error("Pokemon not found");
+    }
+
+    const updatedPokemon = await Pokemon.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedPokemon);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  const updatedPokemon = await Pokemon.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  );
-
-  res.status(200).json(updatedPokemon);
 });
 
 // Delete pokemon
 // @route DELETE /pokemon/:id
 const deletePokemon = asyncHandler(async (req, res) => {
-  const pokemon = await Pokemon.findById(req.params.id);
+  try {
+    const pokemon = await Pokemon.findById(req.params.id);
 
-  if (!pokemon) {
-    res.status(400);
-    throw new Error("Pokemon not found");
+    if (!pokemon) {
+      res.status(400);
+      throw new Error("Pokemon not found");
+    }
+
+    await pokemon.deleteOne();
+    res.status(200).json({ id: req.params.id });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  await pokemon.deleteOne();
-  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
